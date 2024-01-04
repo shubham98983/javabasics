9fe0ada079dcaf8c0c7729cbba089f56ce483108
shubham <sgarg98983@gmail.com>
2024-01-04 21:43:55 +0530

    largest of three number code

---
+++ Largest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class javabasics {
+    public static void main (String args[]){
+    int A = 5 ; int B = 60; int C = 50;
+    if ( (A>=B) && (A>=C) ){
+        System.out.println("A is lagest");
+    } else if ( B>=C ){
+        System.out.println("B is largest");
+    } else {
+        System.out.println("C is largest");
+    }
+     }
+    }
