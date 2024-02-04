4984989c0a1d1fa7b48259199b120068aafa0331
shubham <sgarg98983@gmail.com>
2024-02-04 17:53:02 +0530

    Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,17 @@
+public class Ifelse{
+    public static void main(String args[]){
+        int a =18;
+        if(a>18){
+            System.out.println("You are adult");
+        }
+        if(a<18){
+            System.out.println("You are teenager");
+        }
+        else{
+            System.out.println("you are eighteen");
+        }
+    // More than 1 if statement h to jo shi h vo + else bhi
+    // only if bhi chlegi shi hui to execute
+    // if , else if , else mein jo ek bar shi ho gyi vo aaage chk nhi hogii otherwise at last else.
+    }
+}
